d78973290ab7c7e7e6111fe45a37b973357f7b86
Anh Ha <anh.ha.wz@rvc.renesas.com>
2017-11-02 10:06:55 +0700

    omxvideodec: Support default-pixel-aspect-ratio
    Pixel aspect ratio has affected to image when display, image can
    be smaller or larger than original image base on pixel aspect ratio.
    This patch file will support a default pixel aspect ratio (1:1).
    It means that always keep original image when display.
    
    Add "default-pix-aspect-ratio" into hacks on gstomx.conf to enable it.
    
    refs #138391
    Signed-off-by: Anh Ha <anh.ha.wz@rvc.renesas.com>

--- omx/gstomxvideodec.c
+++ omx/gstomxvideodec.c
@@ -1319,6 +1319,7 @@ gst_omx_video_dec_loop (GstOMXVideoDec * self)
   GstOMXAcquireBufferReturn acq_return;
   GstClockTimeDiff deadline;
   OMX_ERRORTYPE err;
+  GstOMXVideoDecClass *klass = GST_OMX_VIDEO_DEC_GET_CLASS (self);
 
 #if defined (USE_OMX_TARGET_RPI) && defined (HAVE_GST_GL)
   port = self->eglimage ? self->egl_out_port : self->dec_out_port;
@@ -1505,6 +1506,11 @@ gst_omx_video_dec_loop (GstOMXVideoDec * self)
           port_def.format.video.nFrameHeight, self->input_state);
 
       /* Take framerate and pixel-aspect-ratio from sinkpad caps */
+      if (klass->cdata.hacks & GST_OMX_HACK_DEFAULT_PIXEL_ASPECT_RATIO) {
+        /* Set pixel-aspect-ratio is 1/1. It means that always keep
+         * original image when display   */
+        state->info.par_d = state->info.par_n;
+      }
 
       if (!gst_video_decoder_negotiate (GST_VIDEO_DECODER (self))) {
         if (buf)
