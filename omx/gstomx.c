d7337602f0ac3ff870ef90c9c1fa11daffebd105
Anh Ha <anh.ha.wz@rvc.renesas.com>
2017-12-14 11:57:03 +0700

    omxvp8enc: Create new element vp8enc
    This element will use Renesas OMX MC and encode for VP8 video stream.
    
    refs #143346
    
    Signed-off-by: Anh Ha <anh.ha.wz@rvc.renesas.com>
    
    porting from 1.12.2 to 1.14.2
    Signed-off-by: Shinya Saito <ssaito@igel.co.jp>

--- omx/gstomx.c
+++ omx/gstomx.c
@@ -42,6 +42,7 @@
 #include "gstomxwmvdec.h"
 #include "gstomxmpeg4videoenc.h"
 #include "gstomxh264enc.h"
+#include "gstomxvp8enc.h"
 #include "gstomxh263enc.h"
 #include "gstomxh265enc.h"
 #include "gstomxaacdec.h"
@@ -2572,7 +2573,7 @@ static const GGetTypeFunction types[] = {
       , gst_omx_h265_enc_get_type, gst_omx_h265_dec_get_type
 #endif
   , gst_omx_wma_dec_get_type,
-  gst_omx_vp9_dec_get_type
+  gst_omx_vp9_dec_get_type, gst_omx_vp8_enc_get_type
 };
 
 struct TypeOffest
