f1c8be54dfe41671e0125ab7f34b07374ab9aab0
Anh Ha <anh.ha.wz@rvc.renesas.com>
2018-02-08 10:30:44 +0700

    omxh264enc: Support send-eos option
    Set send-eos=true if want to send EOS/EOF data to downstream.
    
    refs #149523
    Signed-off-by: Anh Ha <anh.ha.wz@rvc.renesas.com>
    
    porting from 1.12.2 to 1.14.2
    Signed-off-by: Shinya Saito <ssaito@igel.co.jp>

--- omx/gstomxvideoenc.c
+++ omx/gstomxvideoenc.c
@@ -41,6 +41,9 @@
 #include <OMX_Broadcom.h>
 #include <OMX_Index.h>
 #endif
+#if defined (USE_OMX_TARGET_RCAR)
+#include "gstomxh264enc.h"
+#endif
 #if defined (USE_OMX_TARGET_RCAR) && defined (HAVE_VIDEOENC_EXT)
 #include "OMXR_Extension_vecmn.h"
 #endif
@@ -1533,8 +1536,16 @@ gst_omx_video_enc_handle_output_frame (GstOMXVideoEnc * self, GstOMXPort * port,
       flow_ret =
           gst_video_encoder_finish_frame (GST_VIDEO_ENCODER (self), frame);
     } else {
-      GST_ERROR_OBJECT (self, "No corresponding frame found");
-      flow_ret = gst_pad_push (GST_VIDEO_ENCODER_SRC_PAD (self), outbuf);
+#ifdef USE_OMX_TARGET_RCAR
+      if (GST_IS_OMX_H264_ENC (self) && !GST_OMX_H264_ENC (self)->send_eos) {
+        GST_DEBUG_OBJECT (self, "Skip sending EOS/EOF data to downstream");
+        gst_buffer_unref (outbuf);
+      } else
+#endif
+      {
+        GST_ERROR_OBJECT (self, "No corresponding frame found");
+        flow_ret = gst_pad_push (GST_VIDEO_ENCODER_SRC_PAD (self), outbuf);
+      }
     }
   } else if (frame != NULL) {
     /* Just ignore empty buffers, don't drop a frame for that */
