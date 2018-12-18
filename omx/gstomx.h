ce7b059d2c65e758de64bbf023ab020241e65b1b
Anh Ha <anh.ha.wz@rvc.renesas.com>
2018-12-18 18:45:43 +0900

    omxvideodec: Add hacks to choose default mode
    omx videodecoder support 3 modes to transfer decoded data (OMXBuffer)
    from OMXIL component to GstBuffer:
     - copy mode: Use memcpy to copy decoded data to GstBuffer
     - no-copy mode: Use GstBuffer of BufferPool, this buffer has
    been added mem of OMXBuffer so there is no copy here
     - dmabuf mode: Use GstBufer of BufferPool, but this buffer
    has been added dmabuf_fd.
    
    Normally, default mode is dmabuf mode. This patch will support
    choosing default mode through setting in gstomx.conf
    
    To choose copy mode as default mode, add "use-copy-mode-as-defalut" into
    hacks in gstomx.conf
    Ex:
    [omxh264dec]
    ...
    hacks=no-disable-outport;use-copy-mode-as-default
    
    To choose no-copy mode as default mode, add "use-no-copy-mode-as-default"
    into hacks in gstomx.conf
    Ex:
    [omxh264dec]
    ...
    hacks=no-disable-outport;use-no-copy-mode-as-default
    
    If add both "use-copy-mode-as-default" and use-no-copy-mode-as-default"
    It will be invalid value, the normal mode (dmabuf) will be choosen
    
    refs #138391
    Signed-off-by: Anh Ha <anh.ha.wz@rvc.renesas.com>
    
    porting from 1.12.2 to 1.14.2
    Signed-off-by: Shinya Saito <ssaito@igel.co.jp>

--- omx/gstomx.h
+++ omx/gstomx.h
@@ -3,6 +3,7 @@
  *   Author: Sebastian Dröge <sebastian.droege@collabora.co.uk>, Collabora Ltd.
  * Copyright (C) 2013, Collabora Ltd.
  *   Author: Sebastian Dröge <sebastian.droege@collabora.co.uk>
+ * Copyright (C) 2017, Renesas Electronics Corporation
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -194,6 +195,16 @@ G_BEGIN_DECLS
  * this information to pre-allocate internal buffers for example.
  */
 #define GST_OMX_HACK_PASS_COLOR_FORMAT_TO_DECODER        G_GUINT64_CONSTANT (0x0000000000001000)
+
+/* omx videodecoder support 3 modes (copy, no-copy(use buffer of Bufferpool), dmabuf) to choose.
+ * Default is dmabuf mode. This hack will choose copy mode is default mode
+ */
+#define GST_OMX_HACK_USE_COPY_MODE_AS_DEFAULT                         G_GUINT64_CONSTANT (0x0000000000000800)
+
+/* omx videodecoder support 3 modes (copy, no-copy(use buffer of Bufferpool), dmabuf) to choose.
+ * Default is dmabuf mode. This hack will choose no-copy mode is default mode
+ */
+#define GST_OMX_HACK_USE_NO_COPY_MODE_AS_DEFAULT                      G_GUINT64_CONSTANT (0x0000000000001000)
 
 typedef struct _GstOMXCore GstOMXCore;
 typedef struct _GstOMXPort GstOMXPort;
