06d10ca0c35eb239f8a73541ffc13f28e01d64b8
Anh Ha <anh.ha.wz@rvc.renesas.com>
2018-12-18 18:45:43 +0900

    omxvideodec: Add hacks to choose default mode
    omx videodecoder support 3 modes to transfer decoded data (OMXBuffer)
    from OMXIL component to GstBuffer:
     - copy mode: Use memcpy to copy decoded data to GstBuffer
     - no-copy mode: Use GstBuffer of BufferPool, this buffer has
    been added mem of OMXBuffer so there is no copy here
     - dmabuf mode: Use GstBufer of BufferPool, but this buffer
    has been added dmabuf_fd.
    
    Normally, default mode is dmabuf mode. This patch will support
    choosing default mode through setting in gstomx.conf
    
    To choose copy mode as default mode, add "use-copy-mode-as-defalut" into
    hacks in gstomx.conf
    Ex:
    [omxh264dec]
    ...
    hacks=no-disable-outport;use-copy-mode-as-default
    
    To choose no-copy mode as default mode, add "use-no-copy-mode-as-default"
    into hacks in gstomx.conf
    Ex:
    [omxh264dec]
    ...
    hacks=no-disable-outport;use-no-copy-mode-as-default
    
    If add both "use-copy-mode-as-default" and use-no-copy-mode-as-default"
    It will be invalid value, the normal mode (dmabuf) will be choosen
    
    refs #138391
    Signed-off-by: Anh Ha <anh.ha.wz@rvc.renesas.com>
    
    porting from 1.12.2 to 1.14.2
    Signed-off-by: Shinya Saito <ssaito@igel.co.jp>

--- omx/gstomxvideodec.h
+++ omx/gstomxvideodec.h
@@ -109,6 +109,8 @@ struct _GstOMXVideoDec
   gboolean no_reorder;
   /* Set TRUE to use lossy image compression  */
   gboolean lossy_compress;
+  /* TRUE when set_property() runs */
+  gboolean has_set_property;
 };
 
 struct _GstOMXVideoDecClass
