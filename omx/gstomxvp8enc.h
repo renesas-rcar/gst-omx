a2f0639f3afa6e2a9cc4e0a842a6254c1b8c8a36
Thao Luong <thao.luong.uw@renesas.com>
2021-04-02 19:33:36 +0700

    Update Copyright
    Update year: 2020-2021 for modification source code from Release 1.16.3
    
    refs #313485
    Signed-off-by: Thao Luong <thao.luong.uw@renesas.com>

--- omx/gstomxvp8enc.h
+++ omx/gstomxvp8enc.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017, Renesas Electronics Corporation
+ * Copyright (C) 2017,2020 Renesas Electronics Corporation
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
